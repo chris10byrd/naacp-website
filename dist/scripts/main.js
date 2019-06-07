abbc6cf3915d93f625ef2880b9c632db6c52c038
Christian Byrd <chris10byrd@hotmail.com>
2019-06-07 02:27:42 -0400

    Mobile Responsiveness and Functional Links

---
+++ dist/scripts/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    const hamburgerBtn = document.querySelector('.hamburger');
+    const menu = document.querySelector('.menu');
+
+    hamburgerBtn.addEventListener('click', ()=> {
+        console.log('clicked hamburger');
+        
+    });
+
+});
